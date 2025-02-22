b8897bae6457caf92081db7df3aefb5c8d16ceb8
shay <shay@utah.edu>
2025-02-21 18:19:40 -0700

    lbs matrix, dual quaternion skinning working

--- 403.py
+++ 403.py
@@ -7,6 +7,14 @@ import polyscope as ps
 import polyscope.imgui as gui
 
 dim = 3
+skinning = "dqs"
+assert skinning in ["dqs", "lbs", "lbs_matrix"]
+@wp.kernel
+def rot2quat(R: wp.array(dtype = wp.mat33), q: wp.array(dtype = wp.quat)):
+    i = wp.tid()
+    q[i] = wp.quat_from_matrix(R[i])
+    q[i] /= wp.length(q[i])
+
 class PSViewer: 
     def __init__(self, V, F, W, handles, BE):
         self.V = V
@@ -14,20 +22,23 @@ class PSViewer:
         self.W = W
         self.C = handles
         self.BE = BE
+        self.R = np.zeros((W.shape[1], 3, 3), dtype = float)
 
+        self.q = wp.zeros((W.shape[1],), dtype = wp.quat)
+        self.rotation = wp.zeros((W.shape[1]), dtype = wp.mat33)
         print("BE shape = ", BE.shape)
         self.ps_mesh = ps.register_surface_mesh("mesh", self.V, self.F)
         self.ps_skeleton = ps.register_curve_network("skeleton", self.C, BE, enabled = True, transparency= 1.0, material = "flat")
         ps.set_transparency_mode('simple')
         self.ui_handle = 0
-        self.ui_rest_shape = True
+        self.ui_rest_shape = False
         self.M = igl.lbs_matrix(self.V, self.W)
         self.T = np.zeros((self.M.shape[1], 3), dtype = float)
 
         ps.set_user_callback(self.callback)
 
         self.handles = []
-        self.handle_pos = np.copy(handles)
+        self.handle_pos = np.copy(handles).astype(float)
         self.selected_handles = []
         z3 = np.zeros((1, 3), dtype = float)
         i4 = np.identity(4, dtype = float)
@@ -39,6 +50,8 @@ class PSViewer:
             self.handles[i].set_transform(trans)
             # self.handles[i].set_transform_gizmo_enabled(True)
 
+    def rotation_to_quats(self, R, q):
+        wp.launch(rot2quat, dim = (self.W.shape[1]), inputs = [R, q])
     def callback(self):
         changed, self.ui_handle = gui.SliderInt("handle", self.ui_handle, 0, self.W.shape[1] - 1)
         changed, self.ui_rest_shape = gui.Checkbox("rest shape", self.ui_rest_shape)
@@ -49,12 +62,41 @@ class PSViewer:
             if i >= self.W.shape[1]:
                 break
             trans = hi.get_transform()
-            self.handle_pos[i] = trans[:3, 3]
-            # trans = np.identity(4, dtype = float)
-            trans[: 3, 3] -= self.C[i]
+            self.handle_pos[i] = trans[:3, 3].astype(float)
+            self.R[i] = trans[:3, :3]
+            b = trans[:3, 3]
+            b -= self.R[i] @ self.C[i]
+            trans[: 3, 3] = b
             self.T[i * (dim + 1): (i + 1) * (dim + 1), :] = trans[:3, :].T
         
-        x = self.M @ self.T
+
+        x = np.zeros_like(self.V)
+
+        if skinning == "dqs":
+            # dual quaternion
+            self.rotation.assign(self.R)
+            self.rotation_to_quats(self.rotation, self.q)
+            q = self.q.numpy().astype(np.float64)
+            # dx = self.handle_pos - self.R @ self.C
+            dx = np.zeros_like(self.handle_pos)
+            for i in range(len(self.handles)):
+                dx[i] = self.handle_pos[i] - self.R[i] @ self.C[i]
+            # dx = self.handle_pos
+            dx = dx.astype(np.float64)
+            # # r = self.W @ q
+            
+            x = igl.dqs(self.V, self.W, q, dx)
+        elif skinning == "lbs":
+            # lbs
+            for j in range(len(self.handles)):
+                # lbs 
+                xj = self.R[j] @ (self.V - self.C[j].reshape(-1, 3)).T
+                xj += self.handle_pos[j].reshape(3, -1)
+                xj *= self.W[:, j].reshape(1,- 1)
+                x += xj.T
+        elif skinning == "lbs_matrix":
+            x = self.M @ self.T
+
         if self.ui_rest_shape:
             self.ps_mesh.update_vertex_positions(self.V)
         else:
@@ -117,6 +159,7 @@ def test_bbw():
 
 if __name__ == "__main__":
     ps.init()
+    wp.init()
     V, F, W, C, BE = test_bbw()
     viewer = PSViewer(V, F, W, C, BE)
     ps.show()
