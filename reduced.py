88265adf5d741106c22d5bfbf9b13d5ac80bdcb5
Shay P.C <3180102686@zju.edu.cn>
2025-03-20 00:49:10 -0700

    stashed for debug

--- reduced.py
+++ reduced.py
@@ -1,20 +1,165 @@
 import warp as wp
 from fem.interface import RodComplex
-from stretch import RodBCBase, PSViewer, NewtonState, Triplets, h
+from stretch import RodBCBase, NewtonState, Triplets, h
 from mesh_complex import RodComplexBC, init_transforms
 
 import polyscope as ps
+import polyscope.imgui as gui
 import numpy as np
 
 from geometry.collision_cell import MeshCollisionDetector, collision_eps
 from utils.tobj import import_tobj
-from warp.sparse import bsr_axpy, bsr_set_from_triplets, bsr_zeros
-
+from warp.sparse import bsr_axpy, bsr_set_from_triplets, bsr_zeros, bsr_mm, bsr_transposed, bsr_mv
+from fast_cd import RodLBSWeight
+import os 
+from scipy.linalg import cholesky, cho_solve, cho_factor
 class ReducedRodComplex(RodComplexBC):
     def __init__(self, h, meshes = [], transforms = []):
         super().__init__(h, meshes, transforms)
+
+
+@wp.kernel
+def fill_J_triplets(xcs: wp.array(dtype = wp.vec3), W: wp.array2d(dtype = float), triplets: Triplets):
+    i, j, k = wp.tid()
+    xx = W.shape[0]
+    yy = W.shape[1]
+
+    idx = (i * yy + j) * 4 + k
+    triplets.rows[idx] = i
+    triplets.cols[idx] = j * 4 + k
+    c = float(1.0)
+    if k < 3:
+        c = xcs[i][k]
+    triplets.vals[idx] = wp.diag(wp.vec3(W[i, j] * c))
+
+@wp.kernel
+def inertia_term(z: wp.array(dtype = wp.vec3), zdot: wp.array(dtype = wp.vec3), z0: wp.array(dtype = wp.vec3), dz: wp.array(dtype = wp.vec3), h: float):
+    i = wp.tid()
+    dz[i] = z[i] - (z0[i] + h * zdot[i])
+
+@wp.kernel
+def update_z0_zdot(z: wp.array(dtype = wp.vec3), zdot: wp.array(dtype = wp.vec3), z0: wp.array(dtype = wp.vec3), dz: wp.array(dtype = wp.vec3), h: float):
+    i = wp.tid()
+    z[i] = z0[i] + dz[i]
+    zdot[i] = dz[i] / h
+    z0[i] = z[i]
+
+class ReducedRod(RodLBSWeight):
+    def __init__(self, h):
+        super().__init__()
+        self.h = h
+        self.define_UTKU()
+
+
+        # self.z = wp.zeros((self.n_modes * 4,), dtype = wp.vec3)
+        # self.zdot = wp.zeros_like(self.z)
+        # self.z0 = wp.zeros_like(self.z)
+        # self.dz = wp.zeros_like(self.z)
+        # z - tilde z
+        self.z = np.zeros((self.n_modes * 12,), dtype = float)
+        self.zdot = np.zeros_like(self.z)
+        self.z0 = np.zeros_like(self.z)
+        self.dz = np.zeros_like(self.z)
+
+        for i in range(self.n_modes):
+            self.z0[i * 12: i * 12 + 12] = np.eye(4, 3, dtype = float).reshape(-1)
+            self.z[i * 12: i * 12 + 12] = np.eye(4, 3, dtype = float).reshape(-1)
+    def define_UTKU(self):
+        model = "bar2"
+        Q = None
+        if not os.path.exists(f"data/W_{model}.npy"):
+            _, Q = self.eigs()
+            np.save(f"data/W_{model}.npy", Q)
+        else:
+            Q = np.load(f"data/W_{model}.npy")
+
+        self.weights = wp.array(Q, dtype = float)
         
-    
+        self.n_modes = Q.shape[1]
+        
+        self.J_triplets = Triplets()
+        self.J_triplets.rows = wp.zeros((self.n_modes * self.n_nodes * 4,), dtype = int)
+        self.J_triplets.cols = wp.zeros_like(self.J_triplets.rows)  
+        self.J_triplets.vals = wp.zeros((self.n_modes * self.n_nodes * 4,), dtype = wp.mat33)
+
+        wp.launch(fill_J_triplets, (self.weights.shape[0], self.weights.shape[1], 4), inputs = [self.xcs, self.weights, self.J_triplets])
+
+        # asssemble [0, C^T; C, 0]
+        self.U = bsr_zeros(self.n_nodes, self.n_modes * 4, wp.mat33)        
+        
+        bsr_set_from_triplets(self.U, self.J_triplets.rows, self.J_triplets.cols, self.J_triplets.vals)
+
+        self.uu = self.to_scipy_bsr(self.U)
+        self.kk = self.to_scipy_bsr(self.K_sparse)
+
+        self.uktu = (self.uu.transpose() @ self.kk @ self.uu).toarray()
+        self.sys_matrix = self.h * self.h * self.uktu + np.identity(self.n_modes * 12)
+        
+
+        
+        # self.UTKU = bsr_mm(bsr_transposed(self.U), bsr_mm(self.K_sparse, self.U), alpha = self.h * self.h)
+        
+        # self.sys_matrix = self.to_scipy_bsr(self.UTKU).toarray() + np.identity(self.n_modes * 12)
+
+        self.c, self.lower = cho_factor(self.sys_matrix, lower = True)
+        
+    def step(self):
+        b = self.compute_rhs()
+        dx = self.solve(b)
+        self.update_x0_xdot(dx)
+        
+
+    def solve(self, b):
+        dx = cho_solve((self.c, self.lower), b)
+        return dx
+
+    def update_x0_xdot(self, dx):
+        # self.dz.assign(dx.reshape(-1, 3))
+        # wp.launch(update_z0_zdot, (self.n_modes * 4,), inputs = [self.z, self.zdot, self.z0, self.dz, self.h])
+        self.zdot = dx / self.h
+        self.z = self.z0 + dx
+        self.z0 = self.z
+        
+        
+    def compute_rhs(self):
+        # wp.launch(inertia_term, (self.n_modes * 4), input = [self.z, self.zdot, self.z0, self.dz, self.h])
+        self.dz = self.z - (self.z0 + self.h * self.zdot)
+        b = self.uktu @ self.z * self.h * self.h + self.dz
+        return b
+        # bsr_mv(self.UTKU, self.z, self.dz, alpha = self.h * self.h, beta  = 1.0)
+        # return self.dz.numpy().reshape(-1)
+        
+class PSViewer:
+    def __init__(self, rod: ReducedRod):
+        self.V = rod.xcs.numpy()
+        self.F = rod.F
+
+        self.ps_mesh = ps.register_surface_mesh("rod", self.V, self.F)
+        self.frame = 0
+        self.rod = rod
+        self.ui_pause = True
+        self.animate = False
+    def callback(self):
+        changed, self.ui_pause = gui.Checkbox("Pause", self.ui_pause)
+        self.animate = gui.Button("Step") or not self.ui_pause
+        if gui.Button("Reset"):
+            self.rod.reset()
+            self.frame = 0
+            self.ui_pause = True
+            self.animate = True
+
+        if self.animate: 
+            self.rod.step()
+            # self.V = self.rod.states.x.numpy()
+            self.V = (self.rod.uu @ self.rod.z).reshape(-1, 3)
+            print(f"V = {self.V}")
+            self.ps_mesh.update_vertex_positions(self.V)
+            self.frame += 1
+            
+            print("frame = ", self.frame)
+
+            ps.screenshot(f"output/{self.frame:04d}.jpg")
+
 def reduced_bunny_rain():
     n_meshes = 10
     meshes = ["assets/bunny_5.tobj"] * n_meshes
@@ -29,6 +174,17 @@ def reduced_bunny_rain():
     ps.set_user_callback(viewer.callback)
     ps.show()
 
+def spin():
+    n_meshes = 10
+    meshes = ["assets/bar2.tobj"] * n_meshes
+    
+    transforms = wp.zeros((n_meshes, 4, 4), dtype = float)
+    
+
+    rod = ReducedRod(h)
+    viewer = PSViewer(rod)
+    ps.set_user_callback(viewer.callback)
+    ps.show()
 
 if __name__ == "__main__":  
     ps.init() 
@@ -36,4 +192,5 @@ if __name__ == "__main__":
     wp.config.max_unroll = 0
     wp.init()
     
-    reduced_bunny_rain()
+    # reduced_bunny_rain()
+    spin()
