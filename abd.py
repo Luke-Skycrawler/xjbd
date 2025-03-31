7e97c3347943390d9787c85080a69a8d13d2ce95
shay <haoyang.shi@utah.edu>
2025-03-31 11:45:11 -0700

    abd rain

--- abd.py
+++ abd.py
@@ -1,10 +1,10 @@
 import warp as wp
 import numpy as np
 from stretch import NewtonState, Triplets, h, add_dx, PSViewer
-from mesh_complex import RodComplexBC, init_transforms
+from mesh_complex import RodComplexBC, init_transforms, collision_eps
 import polyscope as ps
 import polyscope.imgui as gui
-
+from tobj import import_tobj
 from scipy.linalg import solve
 from reduced import init_spin
 
@@ -16,25 +16,26 @@ class AffineBodySimulator(RodComplexBC):
         self.A_reduced = np.zeros((self.n_reduced, self.n_reduced))
         self.b_reduced = np.zeros((self.n_reduced, ))
 
-    def reset(self):
-        self.theta = 0.0
-        n_verts = 525
-        wp.copy(self.states.x, self.xcs)
-        wp.copy(self.states.x0, self.xcs)
+    # def reset(self):
+    #     self.theta = 0.0
+    #     n_verts = 525
+    #     wp.copy(self.states.x, self.xcs)
+    #     wp.copy(self.states.x0, self.xcs)
 
-        # pos = self.transforms[:, :3, 3]
-        # positions = wp.array(pos, dtype = wp.vec3)
-        print("init spin")
-        wp.launch(init_spin, (self.n_nodes,), inputs = [self.states])
-        print("xdot = ", self.states.xdot.numpy())
+    #     # pos = self.transforms[:, :3, 3]
+    #     # positions = wp.array(pos, dtype = wp.vec3)
+    #     print("init spin")
+    #     wp.launch(init_spin, (self.n_nodes,), inputs = [self.states])
+    #     print("xdot = ", self.states.xdot.numpy())
 
     def define_U(self):
-
-        x0 = self.xcs.numpy()
+        n_meshes = len(self.meshes_filename)
+        nodes_per_model = self.n_nodes // n_meshes
+        x0 = self.xcs.numpy()[:nodes_per_model]
         v1 = np.ones((x0.shape[0], 4))
         v1[:, :3] = x0
-        self.U = np.kron(x0, np.identity(3,))
-        self.n_reduced = 12
+        self.U = np.kron(np.identity(n_meshes,), np.kron(x0, np.identity(3,)))
+        self.n_reduced = 12 * n_meshes
 
 
 
@@ -61,6 +62,21 @@ class AffineBodySimulator(RodComplexBC):
         return 1.0
 
 
+def bar_rain():
+    n_meshes = 10
+    meshes = ["assets/bar2.tobj"] * n_meshes
+    
+    transforms = wp.zeros((n_meshes, ), dtype = wp.mat44)
+    v, _ = import_tobj(meshes[0])
+    # bb_size = np.max(v, axis = 0) - np.min(v, axis = 0)
+    bb_size = np.ones(3,)
+    wp.launch(init_transforms, (n_meshes,), inputs = [transforms, bb_size[0], bb_size[1], bb_size[2]])
+    print(f"bb_size = {bb_size}")
+    rods = AffineBodySimulator(h, meshes, transforms.numpy())
+    viewer = PSViewer(rods)
+    ps.set_user_callback(viewer.callback)
+    ps.show()
+
 def spin():
     n_meshes = 1
     n_meshes = 1
@@ -79,7 +95,9 @@ def spin():
 
 if __name__ == "__main__":
     ps.init()
-    ps.set_ground_plane_mode("none")
+    # ps.set_ground_plane_mode("none")
+    ps.set_ground_plane_height(-collision_eps)
     wp.config.max_unroll = 1
     wp.init()
-    spin()
+    # spin()
+    bar_rain()
