6cc806bec96ad6606db8a690e4d7dee51f69a84c
shay <haoyang.shi@utah.edu>
2025-06-13 15:58:52 -0700

    remove pins

--- pd_lbfgs.py
+++ pd_lbfgs.py
@@ -4,6 +4,7 @@ from admm_pd import ADMM_PD, ADMMState
 from geometry.collision_cell import MeshCollisionDetector, collision_eps
 from stretch import PSViewer
 import polyscope as ps
+from mesh_complex import set_vx_kernel, init_velocities
 class LBFGS_PD(ADMM_PD):
     def __init__(self, meshes = ["assets/bar2.tobj"], transforms = [np.identity(4)], h = 1e-2):
         super().__init__(meshes, transforms, h)
@@ -11,6 +12,51 @@ class LBFGS_PD(ADMM_PD):
         self.stashed_states = np.zeros((self.m, self.n_nodes * 3))
         self.stashed_gradients = np.zeros_like(self.stashed_states)
         self.n_iters = 10
+        
+        # define_collider 
+        self.static_meshes = None
+        self.define_collider()
+        
+    def define_collider(self): 
+        self.collider = MeshCollisionDetector(self.states.x, self.T, self.indices, self.Bm, ground = 0.0, static_objects = self.static_meshes)
+        self.n_pt = 0
+        self.n_ee = 0
+        self.n_ground = 0
+        
+    def reset(self):
+        n_verts = 4
+        self.theta = 0.0
+        self.frame = 0
+        model = self.meshes_filename[0].split("/")[1].split(".")[0]
+        model_ntets = {
+            "bar2": 525,
+            "tet": 4,
+            "bunny_5": 1356,
+            "bug": 2471,
+            "squishy": 4778,
+            "bunny": 3679, 
+            "windmill": 1350
+        }
+        n_verts = model_ntets[model]
+        wp.copy(self.states.x, self.xcs)
+        wp.copy(self.states.x0, self.xcs)
+        if "assets/tet.tobj" in self.meshes_filename:
+            wp.launch(set_vx_kernel, (self.n_nodes,), inputs = [self.states, n_verts])
+        elif model in["bar2", "bug", "squishy", "bunny", "windmill"]: 
+            # wp.launch(set_velocity_kernel, (self.n_nodes,), inputs = [self.states, n_verts])
+            pass
+        else: 
+            pos = self.transforms[:, :3, 3]
+            positions = wp.array(pos, dtype = wp.vec3)
+            wp.launch(init_velocities, (self.n_nodes,), inputs = [self.states, positions, n_verts])
+
+    def get_constraint_size(self):
+        self.constraints = wp.zeros(0, int)
+        self.n_pinned_constraints = 0
+        return 0
+    
+    def add_constraints(self):
+        pass
 
     def update_x(self):
         '''
